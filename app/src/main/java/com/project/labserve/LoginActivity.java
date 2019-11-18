748dc25f3dc29bceffeaf5d01a7d7c666dc3ac59
Mostafa Abuelnoor <mostafasaher@gmail.com>
2019-11-19 03:52:19 +0400

    Implemented the register and login user with firebase

--- app/src/main/java/com/project/labserve/LoginActivity.java
+++ app/src/main/java/com/project/labserve/LoginActivity.java
@@ -6,28 +6,75 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.example.labserve.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends Activity {
 
-
+    FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginpage);
 
-        EditText emailEditText = findViewById(R.id.emailEditText);
-        EditText passwordEditText = findViewById(R.id.passwordEditText);
+        mAuth= FirebaseAuth.getInstance();
+        final EditText emailEditText = findViewById(R.id.emailEditText);
+        final EditText passwordEditText = findViewById(R.id.passwordEditText);
         Button loginButton = findViewById(R.id.loginButton);
         Button registerButton = findViewById(R.id.registerButton);
 
-        loginButton.setOnClickListener(new View.OnClickListener(){
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mAuth.getCurrentUser();
+                if( mFirebaseUser != null ){
+                    Toast.makeText(LoginActivity.this,"You are logged in",Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"Please Login",Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Toast.makeText(getApplicationContext(), "Finding Labs", Toast.LENGTH_LONG).show();
-                final Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                if(email.isEmpty()){
+                    emailEditText.setError("Please enter email id");
+                    emailEditText.requestFocus();
+                }
+                else  if(password.isEmpty()){
+                    passwordEditText.setError("Please enter your password");
+                    passwordEditText.requestFocus();
+                }
+                else {
+                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(LoginActivity.this,"Login Error, Please Login Again",Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Intent intToHome = new Intent(LoginActivity.this,MainActivity.class);
+                                startActivity(intToHome);
+                            }
+                        }
+                    });
+                }
+
             }
         });
         registerButton.setOnClickListener(new View.OnClickListener(){
@@ -39,5 +86,9 @@ public class LoginActivity extends Activity {
             }
         });
     }
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
 }
--- app/src/main/java/com/project/labserve/RegisterActivity.java
+++ app/src/main/java/com/project/labserve/RegisterActivity.java
@@ -3,27 +3,39 @@ package com.project.labserve;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.labserve.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends Activity {
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         EditText nameEditText = findViewById(R.id.nameEditText);
-        EditText emailEditText = findViewById(R.id.emailEditText);
-        EditText passwordEditText = findViewById(R.id.passwordEditText);
+        final EditText emailEditText = findViewById(R.id.emailEditText);
+        final EditText passwordEditText = findViewById(R.id.passwordEditText);
         CheckBox facultyCheckBox = findViewById(R.id.facultyCheckBox);
         Button cancelButton = findViewById(R.id.cancelButton);
         Button registerButton= findViewById(R.id.registerButton);
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         cancelButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -36,9 +48,33 @@ public class RegisterActivity extends Activity {
         registerButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                // Toast.makeText(getApplicationContext(), "Finding Labs", Toast.LENGTH_LONG).show();
-                final Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if(email.isEmpty()||!email.contains("aus.edu")){
+                    emailEditText.setError("Please enter a valid aus email address");
+                    Toast.makeText(getApplicationContext(), "Please enter a valid aus email address", Toast.LENGTH_LONG).show();
+                    emailEditText.requestFocus();
+                }
+                else if(password.isEmpty()){
+                    emailEditText.setError("Please enter a password");
+                    Toast.makeText(getApplicationContext(), "Please enter a password", Toast.LENGTH_LONG).show();
+                    emailEditText.requestFocus();
+                }
+                else{
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(RegisterActivity.this,"SignUp Unsuccessful, Please Try Again",Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+                            }
+                        }
+                    });
+                }
+
             }
         });
 
